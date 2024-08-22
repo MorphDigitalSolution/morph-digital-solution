5bec3b3fa0a32390c35e09476e7254799de7be9c
Danial <82217321+DanialK007@users.noreply.github.com>
2024-08-22 18:35:25 +0630

    v 1.2.1 - route error solved

---
+++ app/services/business/page.jsx
@@ -0,0 +1,13 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <Header />
+        <div className="h-screen flex items-center justify-center text-4xl font-semibold">Business Registration</div>
+    </div>
+  )
+}
+
+export default page
---
+++ app/services/web/page.jsx
@@ -0,0 +1,13 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <Header />
+        <div className="h-screen flex items-center justify-center text-4xl font-semibold">Web Development</div>
+    </div>
+  )
+}
+
+export default page
